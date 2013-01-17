efaecc169073d59006fdbd6998973b0df1f2671a
Jan Buschtöns <buschtoens@gmail.com>
2013-01-17 20:41:01 +0100

    Fix some warnings

--- src/com/github/cubixcraft/jsonapi/commands/Main.java
+++ src/com/github/cubixcraft/jsonapi/commands/Main.java
@@ -1,16 +1,11 @@
 package com.github.cubixcraft.jsonapi.commands;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.Plugin;
@@ -33,37 +28,37 @@ public class Main extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		this.instance = this;
+		Main.instance = this;
 		this.pm = getServer().getPluginManager();
 		
 		// Check for JSONAPI
 		Plugin checkplugin = this.pm.getPlugin("JSONAPI");
 		if (checkplugin == null) {
-			this.log.severe("[JSONAPI-Commands] Can't load JSONAPI. Get it here: http://dev.bukkit.org/server-mods/jsonapi/");
+			Main.log.severe("[JSONAPI-Commands] Can't load JSONAPI. Get it here: http://dev.bukkit.org/server-mods/jsonapi/");
 			this.pm.disablePlugin(this);
 			return;
 		}
 		
 		// Initialize all the stuff
 		this.jsonapi = (JSONAPI) checkplugin;
-		this.log.info("[JSONAPI-Commands] Hooked into JSONAPI");
+		Main.log.info("[JSONAPI-Commands] Hooked into JSONAPI");
 		
 		this.pm.registerEvents(new CommandListener(), this);
-		this.log.info("[JSONAPI-Commands] Listening on all /commands");
+		Main.log.info("[JSONAPI-Commands] Listening on all /commands");
 		
-		this.log.info("[JSONAPI-Commands] Registering new methods");
+		Main.log.info("[JSONAPI-Commands] Registering new methods");
 		this.jsonapi.getCaller().registerMethods(new APIMethods());
 		
-		this.log.info("[JSONAPI-Commands] Registering commands stream");
+		Main.log.info("[JSONAPI-Commands] Registering commands stream");
 		this.stream = new Stream("commands");
 		this.jsonapi.getStreamManager().registerStream("commands", this.stream);
 		
 		// Load config
-		this.log.info("[JSONAPI-Commands] Loading configuration");
+		Main.log.info("[JSONAPI-Commands] Loading configuration");
 		try {
 			this.config = new ExternalConfig(this.jsonapi.getDataFolder(), "commands.yml");
 		} catch (IOException e) {
-			this.log.severe("[JSONAPI-Commands] Could not load or create command.yml");
+			Main.log.severe("[JSONAPI-Commands] Could not load or create command.yml");
 			e.printStackTrace();
 			this.pm.disablePlugin(this);
 			return;
@@ -74,24 +69,24 @@ public class Main extends JavaPlugin {
 		this.config.reload();
 		
 		// Register commands from config
-		this.log.info("[JSONAPI-Commands] Registering commands");
+		Main.log.info("[JSONAPI-Commands] Registering commands");
 		for (String command : this.config.getStringList("commands")) this.addListener(command);
 		
-		this.log.info("[JSONAPI-Commands] Ready for take-off");
+		Main.log.info("[JSONAPI-Commands] Ready for take-off");
 	}
 	@Override
 	public void onDisable() {}
 	
 	public void addListener(String command) {
-		this.log.info("[JSONAPI-Commands] Add command: " + command);
+		Main.log.info("[JSONAPI-Commands] Add command: " + command);
 		this.listeners.add(command.toLowerCase());
 	}
 	public void removeListener(String command) {
-		this.log.info("[JSONAPI-Commands] Remove command: " + command);
+		Main.log.info("[JSONAPI-Commands] Remove command: " + command);
 		this.listeners.remove(command.toLowerCase());
 	}
 	public void removeAllListeners() {
-		this.log.info("[JSONAPI-Commands] Remove all commands");
+		Main.log.info("[JSONAPI-Commands] Remove all commands");
 		this.listeners.clear();
 	}
 	public Set<String> getListeners() {
@@ -99,7 +94,7 @@ public class Main extends JavaPlugin {
 	}
 	
 	public void emit(String command, String[] args, Player player, PlayerCommandPreprocessEvent event) {
-		this.log.info("[JSONAPI-Commands] " + player.getName() + ": " + event.getMessage());
+		Main.log.info("[JSONAPI-Commands] " + player.getName() + ": " + event.getMessage());
 		this.stream.addMessage(new StreamCommand(command, args, player));
 	}
 }
